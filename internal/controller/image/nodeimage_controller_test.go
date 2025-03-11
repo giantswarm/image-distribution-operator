cd235747575a9968ff4f5c083771a32705f9087d
Antonia von den Driesch <antonia@giantswarm.io>
2025-03-11 17:46:27 +0100

    fix test

--- internal/controller/image/nodeimage_controller_test.go
+++ internal/controller/image/nodeimage_controller_test.go
@@ -51,7 +51,10 @@ var _ = Describe("NodeImage Controller", func() {
 						Name:      resourceName,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
+					Status: imagev1alpha1.NodeImageStatus{
+						Releases: []string{"test-release"},
+						State:    "created",
+					},
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
