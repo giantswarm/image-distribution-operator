18483c258cc08c2e1e495c38fd0be6bd0ef87d30
Antonia von den Driesch <antonia@giantswarm.io>
2025-03-12 15:19:03 +0100

    don't generate rbac for finalizer subresource

--- internal/controller/image/nodeimage_controller.go
+++ internal/controller/image/nodeimage_controller.go
@@ -40,7 +40,6 @@ type NodeImageReconciler struct {
 
 // +kubebuilder:rbac:groups=image.giantswarm.io,resources=nodeimages,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=image.giantswarm.io,resources=nodeimages/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=image.giantswarm.io,resources=nodeimages/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
--- internal/controller/release/release_controller.go
+++ internal/controller/release/release_controller.go
@@ -42,7 +42,6 @@ type ReleaseReconciler struct {
 
 // +kubebuilder:rbac:groups=release.giantswarm.io,resources=releases,verbs=get;list;watch;update;patch
 // +kubebuilder:rbac:groups=release.giantswarm.io,resources=releases/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=release.giantswarm.io,resources=releases/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
