360cbfe72de71ac27a74ee06f44959da150d2fdb
Antonia von den Driesch <antonia@giantswarm.io>
2025-03-13 16:01:00 +0100

    wip

--- pkg/vsphere/client.go
+++ pkg/vsphere/client.go
@@ -2,16 +2,15 @@ package vsphere
 
 import (
 	"context"
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"hash"
 	"io"
 	"net/http"
 	"net/url"
 
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/nfc"
+	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/ovf"
 	"github.com/vmware/govmomi/vim25/types"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -68,145 +67,166 @@ func New(c Config, ctx context.Context) (*Client, error) {
 	}, nil
 }
 
-// UploadImage imports an OVA to vSphere using a task
-func (c *Client) UploadImage(ctx context.Context, imageURL string, expectedSHA256 string, imageName string) error {
-	log := log.FromContext(ctx)
+type TaskConfig struct {
+	ResourcePool  *object.ResourcePool
+	Datastore     *object.Datastore
+	Folder        *object.Folder
+	OVFDescriptor string
+	ImportParams  types.OvfCreateImportSpecParams
+	Host          *object.HostSystem
+}
 
-	log.Info("Starting OVA upload to vSphere", "imageURL", imageURL, "imageName", imageName)
+// CreateImportTask creates an OVA import task and returns the lease
+func (c *Client) CreateImportTask(ctx context.Context, config TaskConfig) (*nfc.Lease, error) {
+	ovfManager := ovf.NewManager(c.vsphere.Client)
 
-	// Stream the OVA while verifying SHA256
-	ovaReader, err := downloadAndVerifyOVA(ctx, imageURL, expectedSHA256)
+	importSpec, err := ovfManager.CreateImportSpec(
+		ctx,
+		config.OVFDescriptor,
+		config.ResourcePool,
+		config.Datastore,
+		&config.ImportParams,
+	)
 	if err != nil {
-		return fmt.Errorf("failed to download and verify OVA.\n%w", err)
+		return nil, fmt.Errorf("failed to create import spec: %w", err)
 	}
-	defer ovaReader.Close()
 
-	// Find the datacenter
+	if importSpec.Error != nil {
+		return nil, fmt.Errorf("import spec contains errors: %+v", importSpec.Error)
+	}
+
+	// Create the import task
+	lease, err := config.ResourcePool.ImportVApp(ctx, importSpec.ImportSpec, config.Folder, config.Host)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start OVA import task: %w", err)
+	}
+
+	return lease, nil
+}
+
+// GetDatacenter returns the datacenter object
+func (c *Client) GetDatacenter(ctx context.Context) (*object.Datacenter, error) {
 	finder := find.NewFinder(c.vsphere.Client, true)
 	dc, err := finder.DatacenterOrDefault(ctx, c.datacenter)
 	if err != nil {
-		return fmt.Errorf("failed to find datacenter %s.\n%w", c.datacenter, err)
+		return nil, fmt.Errorf("failed to find datacenter %s:\n%w", c.datacenter, err)
 	}
 	finder.SetDatacenter(dc)
+	return dc, nil
+}
 
-	// Find the datastore
+// GetDatastore returns the datastore object
+func (c *Client) GetDatastore(ctx context.Context) (*object.Datastore, error) {
+	finder := find.NewFinder(c.vsphere.Client, true)
 	datastore, err := finder.DatastoreOrDefault(ctx, c.datastore)
 	if err != nil {
-		return fmt.Errorf("failed to find datastore %s.\n%w", c.datastore, err)
+		return nil, fmt.Errorf("failed to find datastore %s: %w", c.datastore, err)
+	}
+	return datastore, nil
+}
+
+// GetFolder returns the folder object
+func (c *Client) GetFolder(ctx context.Context, dc *object.Datacenter) (*object.Folder, error) {
+	finder := find.NewFinder(c.vsphere.Client, true)
+
+	// Find the default folder - we might want to do something more sophisticated here
+	finder.SetDatacenter(dc)
+	folder, err := finder.DefaultFolder(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find default folder: %w", err)
+	}
+	return folder, nil
+}
+
+// GetHost returns the host object
+func (c *Client) GetHost(ctx context.Context, hostName string) (*object.HostSystem, error) {
+	finder := find.NewFinder(c.vsphere.Client, true)
+	host, err := finder.HostSystem(ctx, hostName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find host %s: %w", hostName, err)
+	}
+	return host, nil
+}
+
+// GetResourcePool returns the resource pool object
+func (c *Client) GetResourcePool(ctx context.Context, dc *object.Datacenter) (*object.ResourcePool, error) {
+	finder := find.NewFinder(c.vsphere.Client, true)
+
+	// Find the default resource pool - we might want to do something more sophisticated here
+	finder.SetDatacenter(dc)
+	pool, err := finder.DefaultResourcePool(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find default resource pool: %w", err)
+	}
+	return pool, nil
+}
+
+// GetImportParams returns the import parameters
+func (c *Client) ImportOVAFromURL(ctx context.Context, imageURL string, imageName string) (*nfc.Lease, error) {
+	// Fetch the OVF descriptor from the given URL
+	ovfDescriptor, err := FetchOVFDescriptor(ctx, imageURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch OVF descriptor: %w", err)
 	}
 
-	// Use OvfManager to prepare import
-	ovfManager := ovf.NewManager(c.vsphere.Client)
+	// Get the datacenter object
+	dc, err := c.GetDatacenter(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get datacenter: %w", err)
+	}
 
-	// Generate ImportSpec for OVA
-	specParams := types.OvfCreateImportSpecParams{
+	// Get the datastore object
+	datastore, err := c.GetDatastore(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get datastore: %w", err)
+	}
+
+	// Get the folder object
+	folder, err := c.GetFolder(ctx, dc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get folder: %w", err)
+	}
+
+	// Get the resource pool object
+	pool, err := c.GetResourcePool(ctx, dc)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get resource pool: %w", err)
+	}
+
+	// Create the import parameters
+	importParams := types.OvfCreateImportSpecParams{
 		DiskProvisioning: "thin",
 		EntityName:       imageName,
 	}
 
-	importSpec, err := ovfManager.CreateImportSpec(ctx, ovaReader, dc, datastore, specParams)
+	// Create the import task
+	lease, err := c.CreateImportTask(ctx, TaskConfig{
+		ResourcePool:  pool,
+		Datastore:     datastore,
+		Folder:        folder,
+		OVFDescriptor: ovfDescriptor,
+		ImportParams:  importParams,
+	})
 	if err != nil {
-		return fmt.Errorf("failed to create import spec.\n%w", err)
-	}
-	if importSpec.Error != nil {
-		return fmt.Errorf("import spec contains errors: %+v", importSpec.Error)
+		return nil, fmt.Errorf("failed to create import task: %w", err)
 	}
 
-	// Find resource pool and folder for VM placement
-	rp, err := finder.DefaultResourcePool(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to find resource pool.\n%w", err)
-	}
-
-	folder, err := finder.DefaultFolder(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to find default VM folder.\n%w", err)
-	}
-
-	// Create an Import Task
-	lease, err := rp.ImportVApp(ctx, importSpec.ImportSpec, folder, nil)
-	if err != nil {
-		return fmt.Errorf("failed to start OVA import task.\n%w", err)
-	}
-
-	// Monitor Task Progress
-	log.Info("Started OVA import task", "leaseID", lease.Reference().Value)
-	err = waitForLeaseProgress(ctx, log, lease)
-	if err != nil {
-		return fmt.Errorf("OVA import task failed.\n%w", err)
-	}
-
-	log.Info("OVA upload and import completed successfully", "imageName", imageName)
-	return nil
+	return lease, nil
 }
 
-// waitForLeaseProgress tracks the task progress
-func waitForLeaseProgress(ctx context.Context, lease *object.HttpNfcLease) error {
-	log := log.FromContext(ctx)
-	for {
-		info, err := lease.Wait(ctx, nil)
-		if err != nil {
-			return err
-		}
-
-		if info.State == types.HttpNfcLeaseStateDone {
-			log.Info("OVA upload completed")
-			return nil
-		}
-
-		if info.State == types.HttpNfcLeaseStateError {
-			return fmt.Errorf("OVA import task failed: %+v", info.Error)
-		}
-
-		log.Info("Upload in progress", "progress", info.Progress)
-	}
-}
-
-// downloadAndVerifyOVA streams the OVA while computing SHA256
-func downloadAndVerifyOVA(ctx context.Context, imageURL string, expectedSHA256 string) (io.ReadCloser, error) {
+// FetchOVFDescriptor fetches the OVF descriptor from the given URL
+func FetchOVFDescriptor(ctx context.Context, imageURL string) (string, error) {
 	resp, err := http.Get(imageURL)
 	if err != nil {
-		return nil, fmt.Errorf("failed to download OVA.\n%w", err)
+		return "", fmt.Errorf("failed to fetch OVF descriptor: %w", err)
 	}
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("failed to download OVA, HTTP status: %d", resp.StatusCode)
+	defer resp.Body.Close()
+
+	// Read the OVF descriptor into a string
+	ovfDescriptor, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read OVF descriptor: %w", err)
 	}
 
-	// Create SHA256 hasher
-	hasher := sha256.New()
-
-	// Use TeeReader to calculate hash while streaming
-	reader := io.TeeReader(resp.Body, hasher)
-
-	// Pass the reader to the vSphere upload function while verifying
-	return &verifiedReader{
-		reader:      reader,
-		closer:      resp.Body,
-		hasher:      hasher,
-		expectedSHA: expectedSHA256,
-	}, nil
-}
-
-// verifiedReader wraps the streaming OVA download and validates SHA256 at Close()
-type verifiedReader struct {
-	reader      io.Reader
-	closer      io.Closer
-	hasher      hash.Hash
-	expectedSHA string
-}
-
-func (v *verifiedReader) Read(p []byte) (int, error) {
-	return v.reader.Read(p) // Stream data while hashing
-}
-
-func (v *verifiedReader) Close() error {
-	defer v.closer.Close()
-
-	// Compute SHA256 after reading completes
-	computedSHA := hex.EncodeToString(v.hasher.Sum(nil))
-	if computedSHA != v.expectedSHA {
-		return fmt.Errorf("SHA256 mismatch: expected %s, got %s", v.expectedSHA, computedSHA)
-	}
-	return nil
+	return string(ovfDescriptor), nil
 }
